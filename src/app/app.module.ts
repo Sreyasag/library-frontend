413091966a6a4a1c915d86b94cb1f9e5d17f191a
Sreyas <64139020+Sreyasag@users.noreply.github.com>
2022-07-22 14:36:07 +0530

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BooksComponent } from './books/books.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BooksComponent,
+    SignInComponent,
+    SignUpComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
