adb5561cd5785de292bfc92cf8164da2f25c97a8
Sreyas <64139020+Sreyasag@users.noreply.github.com>
2022-07-30 00:24:09 +0530

    signup

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -12,6 +12,7 @@ export class SignInComponent implements OnInit {
   
   @ViewChild('logInForm') logInForm!: NgForm;
   loginError:any;
+  loading=false;
 
   constructor(private auth:AuthService, private router:Router) { }
 
@@ -19,17 +20,20 @@ export class SignInComponent implements OnInit {
   }
 
   onSubmit(){
+    this.loading= true;
     this.auth.login(this.logInForm.value).subscribe(
       {
         next: (data:any)=>{
           if(data.status==='success'){
             this.auth.storeUserData(data.token, data.user);
             this.router.navigate([''])
+            this.loading=false;
           }
         },
         error: (err)=>{
           console.log(err);      
           this.loginError = err.message;
+          this.loading=false;
         } 
       }
     )
